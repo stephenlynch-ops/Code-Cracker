3bf71b65e4eb3e7ba04b622bd248f36a813f1e5d
Stephen Lynch <stephen.lynch17@icloud.com>
2022-03-01 10:37:31 +0000

    FEAT: List functions in script

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+
+function InitialCode() {
+
+}
+
+function runGame() {
+
+}
+
+function collectUsersAnswers() {
+
+}
+
+function gameFailed() {
+
+}
+
+function levelUpdate() {
+
+}
+
+function clearOldAnswers() {
+
+}
+
+function resetGame() {
+
+}
+
+function getPlayerName() {
+
+}
+
+function getInitialPlayerName() {
+
+}
+
+function updateGameHistory() {
+    
+}
