3800de8437d65826a32fb77983502a33cb13175d
Stephen Lynch <stephen.lynch17@icloud.com>
2022-03-01 10:39:11 +0000

    FEAT: Add initial code detail in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,45 @@
+/**
+ * This initial code block is set to run once the DOM has loaded. It calls the user to enter
+ * their name. Identifies the buttons in the DOM. Allows the user to submit an answer by pressing the enter key
+ * It sets the game up at level 1 and calls the game to start.
+ */
+ document.addEventListener("DOMContentLoaded", function() { 
 
-function InitialCode() {
+    getInitialPlayerName();
+    
+    let buttons = document.getElementsByTagName("button");
 
-}
+    for(let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                collectUsersAnswers();
+            } else {
+                resetGame();
+            }
+        });
+    }
+
+    document.getElementById("slot-three").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            collectUsersAnswers();
+        }
+    });
+    document.getElementById("slot-four").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            collectUsersAnswers();
+        }
+    });
+    document.getElementById("slot-five").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            collectUsersAnswers();
+        }
+    });
+
+    document.getElementById("level").innerText = 1;
+    document.getElementById("slot-three").focus();
+
+    runGame();
+});
 
 function runGame() {
 
@@ -36,5 +74,5 @@ function getInitialPlayerName() {
 }
 
 function updateGameHistory() {
-    
+
 }
